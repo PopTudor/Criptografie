206bdebf9a9a0c5b9b0b37b586e05269292d8098
Pop Tudor <tudor08pop@yahoo.com>
2017-01-21 20:27:04 +0200

    done pas 4

--- src/main/java/FractiiContinue.java
+++ src/main/java/FractiiContinue.java
@@ -15,17 +15,18 @@ public class FractiiContinue {
     private BigDecimal x0;
     private BigInteger n;
 
-    private int i = 5;
+    private int i = 6;
     private int[] ai = new int[i];
     private int[] bi = new int[i];
     private int[] bi_2_mod_n = new int[i];
-    private int[] xi = new int[i];
+    private double[] xi = new double[i];
 
     public FractiiContinue(BigInteger n) {
         this.n = n;
         pasUnu(n);
         pasDoi();
         pasTrei();
+        pasPatru();
     }
 
     /**
@@ -47,19 +48,45 @@ public class FractiiContinue {
      * Calculam b0^2 mod n(adica b0^2 - n)
      */
     private void pasDoi() {
-        b0 = b0.modPow(BigInteger.valueOf(2), n);
+//        b0 = b0.modPow(BigInteger.valueOf(2), n);
         System.out.println("b0^2 = " + b0);
+        ai[0] = a0.intValue();
+        bi[0] = b0.intValue();
+        xi[0] = x0.doubleValue();
     }
 
     /**
      * Fie ai = 1/(xi-1). Avem xi = 1/(xi-1)-ai
      */
     private void pasTrei() {
-        for (int j = 0; j < i; j++) {
-            double sqrt_n = (int) Math.sqrt(n.doubleValue());
-            ai[j] = bi[j] = (int) sqrt_n;
-            xi[j] = (int) (sqrt_n - ai[j]);
-            System.out.println("ai");
+        System.out.print("i\t");
+        for (int j = 0; j < i; j++) System.out.printf("%7d ", j);
+        System.out.println();
+        for (int j = 1; j < i; j++) {
+            double sqrt = 1.0 / xi[j - 1];
+            ai[j] = (int) sqrt;
+            xi[j] = sqrt - ai[j];
         }
+        System.out.printf("ai\t");
+        for (int j = 0; j < i; j++) System.out.printf("%7d ", ai[j]);
+        System.out.println();
+        System.out.print("xi\t");
+        for (int j = 0; j < i; j++) System.out.printf("%7.3f ", xi[j]);
+
+        System.out.println();
     }
+
+    private void pasPatru() {
+        for (int j = 1; j < i; j++) {
+            try {
+                bi[j] = ai[j] * bi[j - 1] + bi[j - 2];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                bi[j] = ai[j] * bi[j - 1] + 1;
+            }
+        }
+        System.out.print("bi\t");
+        for (int j = 0; j < i; j++) System.out.printf("%8d", bi[j]);
+    }
+
+
 }
